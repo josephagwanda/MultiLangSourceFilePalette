19d8ebb7ab4c099783a7ff6a85b2fe1241ce4c19
Ozias Gonet <oziasgonet@gmail.com>
2015-12-06 16:40:12 -0500

    Added items

---
+++ src/org/netbeans/modules/multilangsourcefilepalette/python/Decisions/IfStatement.java
@@ -0,0 +1,58 @@
+package org.netbeans.modules.multilangsourcefilepalette.python.Decisions;
+
+import javax.swing.text.BadLocationException;
+import javax.swing.text.JTextComponent;
+import org.netbeans.modules.multilangsourcefilepalette.PaletteUtilities;
+import org.openide.text.ActiveEditorDrop;
+
+public class SingleLineIf implements ActiveEditorDrop {
+
+    private String ifCondition = ""; 
+    private String ifBody = ""; 
+
+    public SingleLineIf() {
+    }
+
+    private String createBody() {
+        String SingleLineIf = String.format("if ( %s ) : %s", getIfCondition(), getIfBody());
+        return SingleLineIf;
+    }
+
+    @Override
+    public boolean handleTransfer(JTextComponent targetComponent) {
+
+        SingleLineIfCustomizer c = new SingleLineIfCustomizer(this, targetComponent);
+        boolean accept = c.showDialog();
+        if (accept) {
+            String body = createBody();
+            try {
+                PaletteUtilities.insert(body, targetComponent);
+            } catch (BadLocationException ble) {
+                accept = false;
+            }
+        }
+
+        return accept;
+
+    }
+
+    public String getIfCondition() {
+        return ifCondition;
+    }
+
+    public void setIfCondition(String ifCondition) {
+        this.ifCondition = ifCondition;
+    }
+
+    public String getIfBody() {
+        return ifBody;
+    }
+
+    public void setIfBody(String ifBody) {
+        this.ifBody = ifBody;
+    }
+
+
+
+}
+
